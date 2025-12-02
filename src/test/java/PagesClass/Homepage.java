6dbba5ec0b20a71ff35b67ab240960dbe8dbb0e1
akash131302 <akashumap1313@gmail.com>
2025-12-02 16:29:13 +0530

    homepage

---
+++ src/test/java/PagesClass/Homepage.java
@@ -0,0 +1,12 @@
+package PagesClass;
+
+public class Homepage {
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
--- src/test/java/dayPractice/Webtable.java
+++ src/test/java/dayPractice/Webtable.java
@@ -1,9 +1,14 @@
 package dayPractice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Webtable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		WebDriver driver=new ChromeDriver();
+		
 
 	}
 
